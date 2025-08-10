60a2c35486f458e9ae95883801d8509840096d21
Leon Schwarzäugl <leon.schwarzaeugl@imba.oeaw.ac.at>
2025-08-10 19:17:50 +0200

    chore[pizauth]: adapt to review

--- modules/home/pizauth.nix
+++ modules/home/pizauth.nix
@@ -8,128 +8,6 @@ let
   inherit (lib) mkOption types;
 
   cfg = config.swarselservices.pizauth;
-
-  renderScopes =
-    scopes:
-    let
-      indent = "  ";
-      quoted = map (s: "\"${s}\"") scopes;
-      joined = lib.concatStringsSep ",\n${indent}${indent}" quoted;
-    in
-    "[\n${indent}${indent}${joined}\n${indent}]";
-
-  renderAccount =
-    let
-      indent = "  ";
-    in
-    name: acc:
-    ''
-      account "${name}" {
-      ${indent}auth_uri = "${acc.authUri}";
-      ${indent}token_uri = "${acc.tokenUri}";
-      ${indent}client_id = "${acc.clientId}";
-      ${indent}client_secret = "${acc.clientSecret}";
-    ''
-    + lib.optionalString (acc.scopes != [ ] && acc.scopes != null) ''
-      ${indent}scopes = ${renderScopes (acc.scopes or [ ])};
-    ''
-    + lib.optionalString (acc.loginHint != "" && acc.loginHint != null) ''
-      ${indent}login_hint = "${acc.loginHint}";
-    ''
-    + lib.optionalString (acc.extraConfig != "" && acc.extraConfig != null) (
-      let
-        indentedExtraConfig = lib.concatMapStringsSep "\n" (
-          line: if line == "" then "" else "${indent}${line}"
-        ) (lib.splitString "\n" acc.extraConfig);
-      in
-      indentedExtraConfig
-    )
-    + ''
-      }
-    '';
-
-  renderedAccounts = lib.concatStringsSep "\n" (
-    lib.mapAttrsToList (name: acc: renderAccount name acc) cfg.accounts
-  );
-
-  configFile = pkgs.writeTextFile {
-    name = "pizauth.conf";
-    text =
-      lib.optionalString (cfg.extraConfig != null && cfg.extraConfig != "") "${cfg.extraConfig}\n"
-      + renderedAccounts;
-  };
-
-  accountOptions = {
-    options = {
-      name = mkOption {
-        type = types.str;
-        readOnly = true;
-        description = ''
-          Unique identifier of the account. This is set to the
-            attribute name of the account configuration.
-        '';
-      };
-
-      authUri = mkOption {
-        type = types.str;
-        description = ''
-          The OAuth2 server's authentication URI.
-        '';
-      };
-
-      tokenUri = mkOption {
-        type = types.str;
-        description = ''
-          The OAuth2 server's token URI.
-        '';
-      };
-
-      clientId = mkOption {
-        type = types.str;
-        description = ''
-          The OAuth2 client ID.
-        '';
-      };
-
-      clientSecret = mkOption {
-        type = types.str;
-        description = ''
-          The OAuth2 client secret.
-        '';
-      };
-
-      scopes = mkOption {
-        type = types.nullOr (types.listOf types.str);
-        description = ''
-          The scopes which the OAuth2 token will give access to. Optional.
-          Note that Office365 requires the non-standard "offline_access" scope to be specified in order for pizauth to be able to operate successfully.
-        '';
-        default = [ ];
-        example = [
-          "https://outlook.office365.com/IMAP.AccessAsUser.All"
-          "https://outlook.office365.com/SMTP.Send"
-          "offline_access"
-        ];
-      };
-
-      loginHint = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = ''
-          The login hint for the account provider. Optional.
-        '';
-      };
-
-      extraConfig = mkOption {
-        type = types.nullOr types.str;
-        default = null;
-        description = ''
-          Additional configuration that will be added to the account configuration. See pizauth.conf(5) for available options.
-        '';
-      };
-    };
-  };
-
 in
 {
   options.swarselservices.pizauth = {
@@ -146,7 +24,78 @@ in
     };
 
     accounts = mkOption {
-      type = types.attrsOf (types.submodule accountOptions);
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            name = mkOption {
+              type = types.str;
+              readOnly = true;
+              description = ''
+                    Unique identifier of the account. This is set to the
+                attribute name of the account configuration.
+              '';
+            };
+
+            authUri = mkOption {
+              type = types.str;
+              description = ''
+                The OAuth2 server's authentication URI.
+              '';
+            };
+
+            tokenUri = mkOption {
+              type = types.str;
+              description = ''
+                The OAuth2 server's token URI.
+              '';
+            };
+
+            clientId = mkOption {
+              type = types.str;
+              description = ''
+                The OAuth2 client ID.
+              '';
+            };
+
+            clientSecret = mkOption {
+              type = types.str;
+              description = ''
+                The OAuth2 client secret.
+              '';
+            };
+
+            scopes = mkOption {
+              type = types.nullOr (types.listOf types.str);
+              description = ''
+                      The scopes which the OAuth2 token will give access to. Optional.
+                Note that Office365 requires the non-standard "offline_access" scope to be specified in order for pizauth to be able to operate successfully.
+              '';
+              default = [ ];
+              example = [
+                "https://outlook.office365.com/IMAP.AccessAsUser.All"
+                "https://outlook.office365.com/SMTP.Send"
+                "offline_access"
+              ];
+            };
+
+            loginHint = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = ''
+                The login hint for the account provider. Optional.
+              '';
+            };
+
+            extraConfig = mkOption {
+              type = types.nullOr types.str;
+              default = null;
+              description = ''
+                Additional configuration that will be added to the account configuration. See pizauth.conf(5) for available options.
+              '';
+            };
+          };
+        }
+      );
       default = { };
       description = "Pizauth accounts that should be configured";
     };
@@ -158,7 +107,59 @@ in
 
     home.packages = [ cfg.package ];
 
-    xdg.configFile."pizauth.conf".source = configFile;
+    xdg.configFile."pizauth.conf".source =
+      let
+        renderScopes =
+          scopes:
+          let
+            indent = "  ";
+            quoted = map (s: "\"${s}\"") scopes;
+            joined = lib.concatStringsSep ",\n${indent}${indent}" quoted;
+          in
+          "[\n${indent}${indent}${joined}\n${indent}]";
+
+        renderAccount =
+          let
+            indent = "  ";
+          in
+          name: acc:
+          ''
+            account "${name}" {
+            ${indent}auth_uri = "${acc.authUri}";
+            ${indent}token_uri = "${acc.tokenUri}";
+            ${indent}client_id = "${acc.clientId}";
+            ${indent}client_secret = "${acc.clientSecret}";
+          ''
+          + lib.optionalString (acc.scopes != [ ] && acc.scopes != null) ''
+            ${indent}scopes = ${renderScopes acc.scopes};
+          ''
+          + lib.optionalString (acc.loginHint != "" && acc.loginHint != null) ''
+            ${indent}login_hint = "${acc.loginHint}";
+          ''
+          + lib.optionalString (acc.extraConfig != "" && acc.extraConfig != null) (
+            let
+              indentedExtraConfig = lib.concatMapStringsSep "\n" (
+                line: if line == "" then "" else "${indent}${line}"
+              ) (lib.splitString "\n" acc.extraConfig);
+            in
+            indentedExtraConfig
+          )
+          + ''
+            }
+          '';
+
+        renderedAccounts = lib.concatStringsSep "\n" (
+          lib.mapAttrsToList (name: acc: renderAccount name acc) cfg.accounts
+        );
+
+        configFile = pkgs.writeTextFile {
+          name = "pizauth.conf";
+          text =
+            lib.optionalString (cfg.extraConfig != null && cfg.extraConfig != "") "${cfg.extraConfig}\n"
+            + renderedAccounts;
+        };
+      in
+      configFile;
 
     systemd.user.services = {
       pizauth = {
